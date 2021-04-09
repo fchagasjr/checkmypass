7ccde0a6a19d97abf278848fc2c8aa169e1311bb
fchagassjr <fcsjr.chagas@gmail.com>
2021-04-09 15:06:41 -0400

    Input docstring on both functions
    Fixed variable name hash -> hashes inside if statement of request_pwned function

--- main.py
+++ main.py
@@ -6,16 +6,34 @@ import hashlib
 
 
 def request_pwned(password_first, password_last):
+    """
+    Send the 5 first characters of the password to API
+    Compare last characters to the list returned
+
+    :param password_first: first 5 characters of the hashed password
+    :param password_last: last characters of the hashed password
+    :return: occurrences of the password
+    """
     url = 'https://api.pwnedpasswords.com/range/' + password_first
     res = requests.get(url)
     hash_pass = (line.split(':') for line in res.text.splitlines())
     for hashes, count in hash_pass:
-        if hash == password_last:
+        if hashes == password_last:
             return count
     return 0
 
 
 def pass_check(password):
+    """
+    Creates a hash version of the password and divides this
+    hash into 2 different parts:
+       -First 5 characters
+       -Last characters
+    Return the occurrences of the password in the API using
+    the request_pwned function.
+
+    :param password: Password to be checked for occurrences
+    """
     password_hashed = hashlib.sha1(password.encode()).hexdigest().upper()
     first5_hash = password_hashed[:5]
     last_hash = password_hashed[5:]
